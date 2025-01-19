b1dab68e4db2760c31c51f086425f140611ede96
Jeff Norris <walker2287@gmail.com>
2025-01-18 21:50:38 -0600

    update snaps

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	testEnvironment: "jsdom",
 	transform: {
 		"^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+		"^.+\\.(css|scss)$": "jest-transform-stub", // Transform CSS/SCSS files
 	},
 	setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
