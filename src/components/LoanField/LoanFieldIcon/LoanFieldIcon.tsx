734be339a266a1dbcfdc73ef3f3ce55ae24df77b
Jeff Norris <walker2287@gmail.com>
2025-01-20 10:00:06 -0600

    fix icon

--- src/components/LoanField/LoanFieldIcon/LoanFieldIcon.tsx
+++ src/components/LoanField/LoanFieldIcon/LoanFieldIcon.tsx
@@ -1,12 +1,12 @@
-import type { State } from "../FieldReducer/FieldReducer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { State } from '../FieldReducer/FieldReducer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
 	faSpinner,
 	faCheckCircle,
 	faExclamationTriangle,
-} from "@fortawesome/free-solid-svg-icons";
+} from '@fortawesome/free-solid-svg-icons'
 
-type LoanFieldIconProps = Omit<State, "isFocused" | "value" | "initialValue">;
+type LoanFieldIconProps = Omit<State, 'isFocused' | 'value' | 'initialValue'>
 
 const LoanFieldIcon = ({
 	isSaving,
@@ -19,20 +19,20 @@ const LoanFieldIcon = ({
 		return (
 			<FontAwesomeIcon
 				icon={faSpinner}
-				className=" ml2 fa-spin gray"
+				className=" ml2 fa-spin light-gray"
 				title="Saving..."
 			/>
-		);
+		)
 	}
 
 	if (hasSaved) {
 		return (
 			<FontAwesomeIcon
 				icon={faCheckCircle}
-				className=" ml2 gray"
+				className=" ml2 light-gray"
 				title="Saved"
 			/>
-		);
+		)
 	}
 
 	if (hasError && isDirty) {
@@ -42,8 +42,8 @@ const LoanFieldIcon = ({
 				className=" ml2 dark-red"
 				title={errorMessage}
 			/>
-		);
+		)
 	}
-};
+}
 
-export default LoanFieldIcon;
+export default LoanFieldIcon
